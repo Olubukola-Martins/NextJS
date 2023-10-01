6c1def718487e2e0140e02ce09197ea086adf387
Olubukola <19bukola97@gmail.com>
2023-10-02 00:40:02 +0100

    refactor: page structer changed, to create the about and profile folders first before including a page in it

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+// You can name it anything e.g const About
+
+ const Page = () => {
+  return <div>My Page About</div>;
+}
+
+export default Page
---
+++ app/profile/page.tsx
@@ -0,0 +1,7 @@
+
+// You can name it anything e.g const Profile
+const Page = () => {
+  return <div>Profile Page</div>;
+};
+
+export default Page;
